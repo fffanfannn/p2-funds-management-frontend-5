15a7aa0881dd544ec2f43dd0d2aefcf99a45bffc
fffanfannn <2295467@edu.vaniercollege.qc.ca>
2023-09-02 21:53:56 -0400

    chart for welcome page

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import ECharts from "vue-echarts";
 
 import App from "./App.vue";
 import router from "./router";
 // import store from "./store";
+
+import ECharts from "vue-echarts";
+import "echarts";
 
 const pinia = createPinia();
 
--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,59 +1,93 @@
 <template>
   <div>
-    <div ref="mychart" id="demoDiv"></div>
     <h1>Welcome page</h1>
     <router-link to="Register">Sign up</router-link>
     <router-link to="Login">Log in</router-link>
+    <div style="display: flex; justify-content: center">
+      <echarts class="echart" :option="option1" />
+      <echarts class="echart" :option="option2" />
+    </div>
+    <router-view></router-view>
   </div>
-  <router-view></router-view>
 </template>
 
 <script>
-// import { ref } from "vue";
-import * as echarts from "echarts";
+import { ref, computed } from "vue";
 export default {
-  mounted() {
-    let myEcharts = echarts.init(this.$refs.mychart);
-    myEcharts.setOption({
-      title: {
-        text: "hello world",
-      },
-      xAxis: {
-        type: "category",
-        data: ["mon", "tue", "wed", "thur", "fri", "sat", "sun"],
-      },
-      yAxis: {
-        type: "value",
-      },
-      series: [
-        {
-          type: "bar",
-          data: [155, 220, 326, 105, 147, 260],
+  name: "WelcomeView",
+  setup() {
+    const data = ref([
+      { value: 50, name: "Mon" },
+      { value: 125, name: "Tue" },
+      { value: 245, name: "Wed" },
+      { value: 147, name: "Thur" },
+      { value: 260, name: "Fri" },
+      { value: 105, name: "Sat" },
+      { value: 326, name: "Sun" },
+    ]);
+
+    setInterval(() => {
+      data.value = data.value.map((item) => ({
+        ...item,
+        value: Math.random() * 100,
+      }));
+    }, 3000);
+
+    const option1 = computed(() => {
+      return {
+        title: {
+          text: "Sample Chart",
         },
-      ],
+        legend: {},
+        xAxis: {
+          data: data.value.map((d) => d.name),
+        },
+        yAxis: {
+          type: "value",
+        },
+        series: [
+          {
+            type: "line",
+            smooth: true,
+            areaStyle: {},
+            data: data.value.map((d) => d.value),
+          },
+        ],
+      };
     });
+
+    const option2 = computed(() => {
+      return {
+        title: {
+          text: "Sample Chart",
+          left: "center",
+        },
+
+        legend: {
+          orient: "vertical",
+          left: "left",
+        },
+        series: [
+          {
+            type: "pie",
+            data: data.value.map((d) => ({
+              value: d.value,
+              name: d.name,
+            })),
+            radius: ["40%", "70%"],
+            roseType: "area",
+          },
+        ],
+      };
+    });
+
+    return { option1, option2 };
   },
 };
-
-// const option = ref({
-//   xAxis: {
-//     type: "category",
-//     data: ["mon", "tue", "wed", "thur", "fri", "sat", "sun"],
-//   },
-//   yAxis: {
-//     type: "value",
-//   },
-//   series: [
-//     {
-//       type: "line",
-//       data: [155, 220, 326, 105, 147, 260],
-//     },
-//   ],
-// });
 </script>
 
 <style>
-#demoDiv {
+.echart {
   width: 600px;
   height: 400px;
   border: 1px solid #ccc;
